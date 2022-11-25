6341386b7cd1b703f095a659f621efda23c317ac
Adam Argyle <atom@argyleink.com>
2022-11-24 22:14:17 -0800

    added nav component

---
+++ components/Nav/Nav.tsx
@@ -0,0 +1,10 @@
+import { JSX } from 'preact'
+
+export default function Nav() {
+  return (
+    <nav class="page-nav">
+      <img src="favicon.svg" width="40"/>
+      <h2>ArgyleInk</h2>
+    </nav>
+  )
+}
--- components/PostItem/PostItem.tsx
+++ components/PostItem/PostItem.tsx
@@ -1,5 +1,5 @@
 import { JSX } from 'preact'
-import { Post } from '../../utils/posts.ts'
+import { Post } from '~/utils/posts.ts'
 
 export default function PostItem(props: { post: Post }) {
   const { post } = props

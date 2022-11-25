f12a54c9611586dfc5ceabc39d0a7ac1d4b95766
Adam Argyle <atom@argyleink.com>
2022-11-24 21:08:42 -0800

    prod builds the stylesheet

--- main.ts
+++ main.ts
@@ -6,6 +6,8 @@
 
 import { start } from '$fresh/server.ts'
 import manifest from './fresh.gen.ts'
+import {buildStyles} from './utils/styles.build.ts'
+
+buildStyles()
 
 await start(manifest, { port: 3030 });
-

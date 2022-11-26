f833c724a64b1ce25eab4371d45f9afa7ad7d1cf
Adam Argyle <atom@argyleink.com>
2022-11-25 18:29:30 -0800

    date utils module

---
+++ utils/dates.ts
@@ -0,0 +1,8 @@
+import { formatDistance, isToday } from 'npm:date-fns'
+// import { en } from 'npm:date-fns/locale'
+
+export function relDate(date) {
+  return isToday(new Date(date), new Date())
+    ? 'today'
+    : formatDistance(new Date(date), new Date())
+}

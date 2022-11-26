b9e86c031c901d1a61f81af76e8c5952e46257ef
Adam Argyle <atom@argyleink.com>
2022-11-25 22:07:41 -0800

    nav title accepts props

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,8 +1,8 @@
-export default function Nav() {
+export default function Nav(props) {
   return (
     <nav class="page-nav">
       <img src="favicon.svg" width="40" alt="A skull icon icon with a flipped up bill on a hotpink hat."/>
-      <h1>Home</h1>
+      <h1>{props?.title || 'Home'}</h1>
       <a href="/rss">
         <svg class="filled-icon" width="48" height="48" viewBox="0 0 24 24">
           <use href="#rss"/>

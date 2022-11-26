22dcf492b37a637f01e79bb504f8bcd994ec84be
Adam Argyle <atom@argyleink.com>
2022-11-25 19:21:16 -0800

    update the post image

--- islands/Pic.tsx
+++ islands/Pic.tsx
@@ -2,7 +2,6 @@ import { useState } from "preact/hooks"
 import { IS_BROWSER } from '$fresh/runtime.ts'
 
 const urlbase = 'https://res.cloudinary.com/dnpmdb8r8/image/upload'
-const folder = 'argyleink'
 const lqip = 'w_240,e_blur:2000,c_thumb,f_auto,q_auto:low'
 
 export interface iPic {
@@ -14,14 +13,15 @@ export default function Pic(props: iPic) {
   const [loaded, setLoaded] = useState(0)
 
   const state = {loaded: false}
-  const full = [urlbase, folder, props.src].join('/')
-  const placeholder = [urlbase, lqip, folder, props.src].join('/')
+  const full = [urlbase, props.src].join('/')
+  const placeholder = [urlbase, lqip, props.src].join('/')
 
   if (IS_BROWSER) {
     const preloader = new Image()
     preloader.src = full
     preloader.onload = () => setLoaded(1)  
   }
+  // else todo: ping the api for the height/width and inline it
 
   return (
     <img 
--- posts/first.md
+++ posts/first.md
@@ -2,9 +2,16 @@
 title: This is my first blog post!
 published_at: 2022-11-26
 snippet: This is an excerpt of my first blog post.
-hero: Dream_TradingCard_19_ybgk1f.jpg
+hero: v1669432177/argyleink/text-emphasized.png
 hero_alt: Made this with AI
 ---
 
-### hi
-foo
+a rad [#CSS](#) one-liner for your hot topics
+
+```css
+h1 {
+  text-emphasis-style: "🔥";
+}
+```
+
+try it on [Codepen](https://codepen.io/argyleink/pen/YzveomK)

68db42bbea75bd61bc71ea698bf7ec4ccb010c11
Adam Argyle <atom@argyleink.com>
2022-11-26 19:49:55 -0800

    started utils file

--- components/Tags/Tags.tsx
+++ components/Tags/Tags.tsx
@@ -1,7 +1,9 @@
 export default function Tags(props) {
   return (
-    <div class="tags">{props.tags.map(tag => (
-      <span class="tag">{tag}</span>
-    ))}</div>
+    <div class="tags inline-wrap">
+      {props.tags.map(tag => (
+        <span class="tag">{tag}</span>
+      ))}
+    </div>
   )
 }

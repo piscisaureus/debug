f3a4a1f047732ac54212f451ffe3d57f73682a65
Adam Argyle <atom@argyleink.com>
2022-11-26 16:45:30 -0800

    make nav avatar icon a link

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,7 +1,9 @@
 export default function Nav(props) {
   return (
     <nav class="page-nav">
-      <img src="favicon.svg" width="40" alt="A skull icon icon with a flipped up bill on a hotpink hat."/>
+      <a href="/">
+        <img src="favicon.svg" width="40" alt="A skull icon icon with a flipped up bill on a hotpink hat."/>
+      </a>
       <h1>{props?.title || 'Home'}</h1>
       <a href="/rss">
         <svg class="filled-icon" width="48" height="48" viewBox="0 0 24 24">

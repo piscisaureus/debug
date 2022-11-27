7ecf2e441e90414ed9042594f1e8e8594ccd9e75
Adam Argyle <atom@argyleink.com>
2022-11-26 22:27:47 -0800

    accepts more props

--- islands/Pic.tsx
+++ islands/Pic.tsx
@@ -4,15 +4,11 @@ import { IS_BROWSER } from '$fresh/runtime.ts'
 const urlbase = 'https://res.cloudinary.com/dnpmdb8r8/image/upload'
 const lqip = 'w_240,e_blur:2000,c_thumb,f_auto,q_auto:low'
 
-export interface iPic {
-  alt: string;
-  src: string;
-}
-
-export default function Pic(props: iPic) {
+export default function Pic(props) {
   const [loaded, setLoaded] = useState(0)
 
   const state = {loaded: false}
+
   const { full, placeholder } = picPaths(props.src)
 
   if (IS_BROWSER) {
@@ -20,7 +16,12 @@ export default function Pic(props: iPic) {
     preloader.src = full
     preloader.onload = () => setLoaded(1)  
   }
-  // else todo: ping the api for the height/width and inline it
+  // else {
+  //   const imgRes = await fetch(full)
+  //   const sizing = await sizeOf(full)
+  //   props.height = sizing.height
+  //   props.width = sizing.width
+  // }
 
   return (
     <img 
@@ -28,7 +29,9 @@ export default function Pic(props: iPic) {
       data-state={loaded ? 'loaded' : 'loading'}
       alt={props.alt}
       src={loaded ? full : placeholder} 
-      style={!loaded && 'inline-size: 100%'}
+      width={props.width}
+      height={props.height}
+      className={props.class}
     />
   )
 }

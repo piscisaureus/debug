be14e7e892a6faf7a3cd516cf8ded2bfa389d056
Adam Argyle <atom@argyleink.com>
2022-11-27 19:26:28 -0800

    rename effect

--- components/MediaScroller/MediaScroller.tsx
+++ components/MediaScroller/MediaScroller.tsx
@@ -1,11 +1,11 @@
 export default function MediaScroller({media}) {
   return (
     <div class="MediaScroller">
-      <div></div>
+      <overscroll-effect></overscroll-effect>
       {media.map(item => (
         <img src={item.src} alt={item.alt}/>
       ))}
-      <div></div>
+      <overscroll-effect></overscroll-effect>
     </div>
   )
 }

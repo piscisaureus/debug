9b550aaeedbdeb49fc6d2df97ae0b3a093d40e75
Adam Argyle <atom@argyleink.com>
2022-11-27 20:58:38 -0800

    add custom transforms to image

--- islands/Pic.tsx
+++ islands/Pic.tsx
@@ -2,26 +2,20 @@ import { useState } from "preact/hooks"
 import { IS_BROWSER } from '$fresh/runtime.ts'
 
 const urlbase = 'https://res.cloudinary.com/dnpmdb8r8/image/upload'
-const lqip = 'w_240,e_blur:2000,c_thumb,f_auto,q_auto:low'
+const lqip = 'e_blur:2000,c_thumb,f_auto,q_auto:low'
 
 export default function Pic(props) {
   const [loaded, setLoaded] = useState(0)
 
   const state = {loaded: false}
 
-  const { full, placeholder } = picPaths(props.src)
+  const { full, placeholder } = picPaths(props)
 
   if (IS_BROWSER) {
     const preloader = new Image()
     preloader.src = full
     preloader.onload = () => setLoaded(1)  
   }
-  // else {
-  //   const imgRes = await fetch(full)
-  //   const sizing = await sizeOf(full)
-  //   props.height = sizing.height
-  //   props.width = sizing.width
-  // }
 
   return (
     <img 
@@ -36,9 +30,9 @@ export default function Pic(props) {
   )
 }
 
-export function picPaths(src) {
+export function picPaths({src, cloudinary}) {
   return {
-    full:         [urlbase, src].join('/'),
+    full:         [urlbase, cloudinary, src].join('/'),
     placeholder:  [urlbase, lqip, src].join('/'),
   }
 }

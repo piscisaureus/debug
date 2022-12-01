f8b0cd2767c726dc454140e0430b004fa251325a
Adam Argyle <atom@argyleink.com>
2022-11-30 22:33:48 -0800

    add 404 todo

--- routes/[name].tsx
+++ routes/[name].tsx
@@ -4,12 +4,12 @@ import { Head } from '$fresh/runtime.ts'
 import { getPost, Post } from '~/utils/posts.ts'
 import PageMeta from '~/components/PageMeta.tsx'
 import PostDetail from '~/components/Posts/Detail.tsx'
-
+// todo: custom 404 page
 export const handler: Handlers<Post[]> = {
   async GET(_req, ctx) {
     const post = await getPost(ctx.params.name)
     if (!post)
-      return new Response("Post not found", { status: 404 });
+      return new Response("Post not found", { status: 404 })
     return ctx.render(post)
   }
 }

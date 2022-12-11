ce9f98a12e869a46479a4351c8fd34bd6c1156fa
Adam Argyle <atom@argyleink.com>
2022-12-10 21:41:30 -0800

    back to none specific dates to avoid confusion

--- posts/layouts-are-loops.md
+++ posts/layouts-are-loops.md
@@ -1,7 +1,7 @@
 ---
 type: note
 persona: argyleink
-published_at: 2022-12-11T05:07:59.248
+published_at: 2022-12-10
 tags: 
   - css
 ---
--- utils/dates.ts
+++ utils/dates.ts
@@ -2,5 +2,7 @@ import { formatDistance, isToday } from 'https://esm.sh/date-fns'
 // import { en } from 'https://unpkg.com/date-fns/esm/locale'
 
 export function relDate(date:Date) {
-  return formatDistance(new Date(date), new Date()) + ' ago'
+  return isToday(new Date(date), new Date())
+    ? 'today'
+    : formatDistance(new Date(date), new Date()) + ' ago'
 }

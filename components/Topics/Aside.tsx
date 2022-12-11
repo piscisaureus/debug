915f9aa6d39e3201a328abb131a330944ab7a991
Adam Argyle <atom@argyleink.com>
2022-12-10 20:49:26 -0800

    responsive topic filters

--- components/Topics/Aside.tsx
+++ components/Topics/Aside.tsx
@@ -5,6 +5,10 @@ export default function Aside() {
 
   return (
     <aside class="TopicsAside">
+      <a id="all" href="#all">
+        all
+        <span>{tags.size}</span>
+      </a>
       {Array.from(tags.keys()).filter(tag => tag !== 'test').map(tag => 
         <a id={tag} href={`#${tag}`}>
           {tag}
@@ -13,7 +17,7 @@ export default function Aside() {
       )}
       <style dangerouslySetInnerHTML={{ __html: Array.from(tags.keys()).map(topic => {
         return `
-          html:has(#${topic}:target) .PostItem:not(:has([data-topic="${topic}"])) {
+          html:has(#${topic}:target) .PostList > li:not(:has([data-topic="${topic}"])) {
             display: none;
           }`
       }).join('\n')}}/>

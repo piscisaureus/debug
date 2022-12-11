6ffeff7f43e7df07f978aed76d50f883992b468b
Adam Argyle <atom@argyleink.com>
2022-12-10 21:14:39 -0800

    always use relative time

--- utils/dates.ts
+++ utils/dates.ts
@@ -2,7 +2,5 @@ import { formatDistance, isToday } from 'https://esm.sh/date-fns'
 // import { en } from 'https://unpkg.com/date-fns/esm/locale'
 
 export function relDate(date:Date) {
-  return isToday(new Date(date), new Date())
-    ? 'today'
-    : formatDistance(new Date(date), new Date()) + ' ago'
+  return formatDistance(new Date(date), new Date()) + ' ago'
 }

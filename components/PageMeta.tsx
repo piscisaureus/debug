b9cfc4c2b82d01741b03cff8b56d17a11621184b
Adam Argyle <argyle@google.com>
2022-12-13 14:56:59 -0800

    remove service worker for now

--- components/PageMeta.tsx
+++ components/PageMeta.tsx
@@ -34,13 +34,13 @@ export default function PageMeta(props) {
 
       <script async src="/toast.js"/>
 
-      <script dangerouslySetInnerHTML={{ __html: `
+      {/* <script dangerouslySetInnerHTML={{ __html: `
         if ('serviceWorker' in navigator) {
           window.addEventListener('load', function() {
             navigator.serviceWorker.register('/service-worker.js');
           });
         }   
-      `}}/>
+      `}}/> */}
 
          {
            "prerender": [

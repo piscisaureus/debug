462120efdd4af67af8a073773c0ab4572c4e505d
Adam Argyle <atom@argyleink.com>
2022-12-13 23:01:01 -0800

    adds header to toc

--- components/TableOfContents/TableOfContents.tsx
+++ components/TableOfContents/TableOfContents.tsx
@@ -1,6 +1,7 @@
 export default function TableOfContents({toc}: {toc: []}) {
   return (
     <aside class="TableOfContents">
+      <h5>Table of contents</h5>
       {toc.map(({text, level}) => (
         <a href={'#'+text.toLowerCase().replaceAll(' ', '-')}>{text}</a>
       ))}

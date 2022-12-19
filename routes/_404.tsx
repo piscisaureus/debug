ef8191cca781cb05ace5b76ec7aa9fae2e9dae71
Adam Argyle <argyle@google.com>
2022-12-19 12:49:57 -0800

    proper 404 head tag and better comm

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,28 +1,35 @@
-import { UnknownPageProps } from "$fresh/server.ts";
+import { UnknownPageProps } from '$fresh/server.ts'
+import { Head } from '$fresh/runtime.ts'
 
 export default function NotFoundPage({ url }: UnknownPageProps) {
   return (
     <>
-      <link rel="stylesheet" href="https://unpkg.com/open-props"/>
-      <link rel="stylesheet" href="https://unpkg.com/open-props/normalize.min.css"/>
-      <style dangerouslySetInnerHTML={{ __html: `
-        body {
-          display: grid;
-          place-content: center;
-          place-items: center;
-          gap: var(--size-fluid-3);
-        }
+      <Head>
+        <link rel="stylesheet" href="https://unpkg.com/open-props"/>
+        <link rel="stylesheet" href="https://unpkg.com/open-props/normalize.min.css"/>
+        <style dangerouslySetInnerHTML={{ __html: `
+          body {
+            display: grid;
+            place-content: center;
+            place-items: center;
+            gap: var(--size-fluid-3);
+          }
 
-        p {
-          padding-block: var(--size-4);
-          padding-inline: var(--size-6);
-          background: var(--surface-2);
-          box-shadow: var(--shadow-6);
-          font-size: var(--font-size-3);
-        }
-      `}}/>
+          p {
+            padding-block: var(--size-4);
+            padding-inline: var(--size-6);
+            background: var(--surface-2);
+            box-shadow: var(--shadow-6);
+            font-size: var(--font-size-3);
+          }
+
+          code {
+            background: var(--surface-3);
+          }
+        `}}/>
+      </Head>
       <img src="/favicon.svg" width='150' />
-      <p>Yo, sorry, this page wasn't found: {url.pathname}</p>
+      <p>Yo, sorry, <code>{url.pathname}</code> wasn't found</p>
     </>
   )
 }

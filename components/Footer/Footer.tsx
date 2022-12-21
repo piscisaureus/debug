ddaae801fe6119db0cb51a9f2aa7b94b5b42ec09
Adam Argyle <argyle@google.com>
2022-12-21 10:31:55 -0800

    social links add rel for indielogin

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,13 +1,13 @@
 export default function Footer() {
   return (
     <footer class="Footer center-center">
-      <a href="https://twitter.com/argyleink" class="icon-button">
+      <a href="https://twitter.com/argyleink" rel="me" class="icon-button">
         <div class="sr-only">Follow me on Twitter</div>
         <svg aria-hidden class="filled-icon" width="24" height="24" viewBox="0 0 30 30">
           <use href="#icon.twitter"/>
         </svg>
       </a>
-      <a href="https://github.com/argyleink" class="icon-button">
+      <a href="https://github.com/argyleink" rel="me" class="icon-button">
         <div class="sr-only">Follow me on Github</div>
         <svg aria-hidden class="filled-icon" width="24" height="24" viewBox="0 0 24 24">
           <use href="#icon.github"/>

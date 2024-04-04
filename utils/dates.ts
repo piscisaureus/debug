c3584b8f52a0004a49c0eb20589ec6c07aab8105
Bert Belder <bertbelder@gmail.com>
2024-04-04 16:13:23 -0700

    faster?

--- utils/dates.ts
+++ utils/dates.ts
@@ -1,9 +1,9 @@
-import { formatDistance, isToday } from 'date-fns/esm/index.js'
-import * as Locales from 'date-fns/esm/locale/index.js'
-import { getLocale } from '~/utils/locale.ts'
+//import { formatDistance, isToday } from "date-fns/esm/index.js";
+//import * as Locales from "date-fns/esm/locale/index.js";
+import { getLocale } from "~/utils/locale.ts";
 
-export function relDate(date:Date) {
-  const {code} = getLocale()
-
-  return formatDistance(new Date(date), new Date(), {locale: Locales[code]})
+export function relDate(date: Date) {
+  const { code } = getLocale();
+  return date.toString();
+  //return formatDistance(new Date(date), new Date(), {locale: Locales[code]})
 }
